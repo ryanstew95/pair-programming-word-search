f8e13b63f6684770dd140bb5732bd4d8d13f5cb3
ryanstew95 <127888213+ryanstew95@users.noreply.github.com>
2023-05-19 00:05:46 -0300

    Update wordsearch.js

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,37 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function(matrix) {
+  let transposed = [];
+  for (let i = 0; i < matrix[0].length; i++) {
+    transposed[i] = [];
+    for (let j = 0; j < matrix.length; j++) {
+      transposed[i][j] = matrix[j][i];
     }
-}
+  }
+  return transposed;
+};
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+
+  if (letters.length === 0) {
+    return false;
+  }
+  const lettersV2 = transpose(letters);
+  const verticalJoin = lettersV2.map(ls => ls.join(''));
+
+
+  for (l of verticalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+
+  }
+  const horizontalJoin = letters.map(ls => ls.join(''));
+
+  for (l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+
+  }
+  return false;
+};
+module.exports = wordSearch;
